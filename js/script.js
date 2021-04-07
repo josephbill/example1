f1bed3071553b8d1450b33fc2f72703f0ab140f4
josephbill <josephbill00@gmail.com>
2021-04-07 12:56:51 +0300

    added elements part 2 and intro to css and js code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+	document.getElementById("changeColor").addEventListener('click', function(){
+			// alert("works");
+              document.getElementById("bodyPage").style.backgroundColor = "yellow";
+		})
+
+		document.getElementById("sizeChange").addEventListener('click', function(){
+			  document.getElementById("paraEmpty").innerHTML = "was empty now i have content";
+		})
